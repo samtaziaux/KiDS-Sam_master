3b934245fe022a085d30d600d7495a0c15c7dc20
margotbrouwer <margot.brouwer@gmail.com>
2015-08-14 16:43:19 +0200

    [Not working] implementing ESD code into the pipeline

--- esd_production/esd_utils.py
+++ esd_production/esd_utils.py
@@ -1,5 +1,6 @@
 import os
 from glob import glob
+import numpy as np
 #from ConfigParser import SafeConfigParser
 
 def read_config(config_file, version='0.5.7', Om=0.315, Ol=0.685, Ok=0, h=0.7,
@@ -17,21 +18,19 @@ def read_config(config_file, version='0.5.7', Om=0.315, Ol=0.685, Ok=0, h=0.7,
         if line.replace(' ', '').replace('\t', '')[0] == '#':
             continue
         line = line.split()
+        if line == []:
+            continue
+
         ## General settings
         if line[0] == 'KiDS_version':
-<<<<<<< HEAD
             kidsversion = line[1]
         elif line[0] == 'GAMA_version':
             gamaversion = line[1]
-=======
-            kids_version = line[1]
         elif line[0] == 'KiDS_path':
             kids_path = line[1]
-        elif line[0] == 'GAMA_version':
-            gama_version = line[1]
         elif line[0] == 'GAMA_path':
             gama_path = line[1]
->>>>>>> 23fa859e75f0d2b89876589d0a878919b09abbd9
+
         # Cosmology
         elif line[0] == 'Om':
             Om = float(line[1])
@@ -43,52 +42,55 @@ def read_config(config_file, version='0.5.7', Om=0.315, Ol=0.685, Ok=0, h=0.7,
             h = float(line[1])
 
         ## ESD production
+
         # Algorithm
-        elif line[0] == 'output_folder':
+        elif line[0] == 'ESD_output_folder':
             folder = line[1]
-        elif line[0] == 'output_filename':
+        elif line[0] == 'ESD_output_filename':
             filename = line[1]
-        elif line[0] == 'purpose':
+        elif line[0] == 'ESD_purpose':
             purpose = line[1]
         elif line[0] == 'Rbins':
             Rbins = line[1]
         elif line[0] == 'ncores':
             ncores = int(line[1])
+
         # Lens selection
         elif line[0] == 'lensid_file':
             lensid_file = line[1]
         elif line[0] == 'group_centre':
             group_centre = line[1]
         elif line[0] == 'lens_binning':
-            binparam = {line[1]: [int(i) for i in line[2].split(',')]}
+            bins = np.array([float(i) for i in line[2].split(',')])
+            lens_binning = {line[1]: bins}
+            
         elif line[0][:10] == 'lens_param':
             param = line[1]
-            bins = line[2].split(',')
+            bins = np.array([float(i) for i in line[2].split(',')])
             if len(bins) == 1:
                 bins = bins[0]
             lens_selection[param] = bins
+        elif line[0] == 'kids_blinds':
+            blindcats = line[1].split(',')
+        
+        # Source selection
         elif line[0][:9] == 'src_param':
             param = line[1]
-            bins = line[2].split(',')
+            bins = np.array([float(i) for i in line[2].split(',')])
             if len(bins) == 1:
                 bins = bins[0]
             src_selection[param] = bins
-<<<<<<< HEAD
-    out = (purpose, folder, filename, kidsversion, gamaversion,
-            ncores, Rbins, src_selection,
-            lens_selection, group_centre, lensid_file, binparam,
-            Om, Ol, Ok, h)
 
-=======
+
     #if kids_version and not kids_path:
         #kids_path =
     #if gama_version and not gama_path:
         #gama_path =
-    out = (kids_path, gama_path, Om, Ol, Ok, h,
-           folder, filename, purpose, Rbins, ncores,
-           lensid_file, group_centre,
-           binparam, lens_selection, src_selection)
->>>>>>> 23fa859e75f0d2b89876589d0a878919b09abbd9
+
+    out = (kids_path, gama_path,
+            Om, Ol, Ok, h,
+            folder, filename, purpose, Rbins, ncores,
+            lensid_file, group_centre, lens_binning, lens_selection,
+            src_selection, blindcats)
+
     return out
-
-
--- esd_production/margot/run_shearcode.py
+++ esd_production/margot/run_shearcode.py
@@ -4,8 +4,6 @@
 
 import pyfits
 import numpy as np
-import distance
-import itertools
 import sys
 import os
 import time
@@ -13,159 +11,133 @@ from astropy import constants as const, units as u
 import numpy.core._dotblas
 import subprocess as sub
 import shlex
-from decimal import *
 
 # local
+import distance
 import esd_utils
+
 
 start_tot = time.time()
 
 # Important constants
 inf = np.inf
 
-# Taking the input parameters
 
-config_file = '/data2/brouwer/shearprofile/KiDS-GGL/ggl.config'
+def define_runparams(purpose, lens_binning, ncores, blindcats):
 
-def import_config(config_filename):
-    # Default values
-    info = esd_utils.read_config(config_file)
+    # The number of catalogues that will be ran
+    if 'random' in purpose:
+        nruns = 100
+        if 'combine' in purpose:
+            nruns = 0
+    else:
+        nruns = 1
 
-    # Input for the codes
-	[purpose, folder, filename, kidsversion, gamaversion,
-    ncores, Rbins, src_selection,
-    lens_selection, group_centre, lensid_file, binparam,
-    Om, Ol, Ok, h] = [info[i] for i in xrange(len(info))]
+    # Binnning information of the groups
+    binname = lens_binning.keys()[0]
+    if binname != 'None': # If there is binning
+        obsbins = lens_binning[binname]
+        nobsbins = len(obsbins)
+    else: # If there is no binning
+        nobsbins = 1
+    nobsbin = nobsbins # Starting value
 
-def main():
-	
-	print '\n \n \n \n \n \n \n \n \n \n'
+    # Prepare the values for Ncores and Nobsbins
+    nsplits = ncores/nobsbins
+    if nsplits == 0:
+        nsplits = 1
+    nsplit = 1 # Starting value
 
-	print 'Running:', purpose
+    # Names of the blind catalogs
+    blindcat = blindcats[0]
 
-	# Binnning information of the groups
-    binparam = lens_selection.keys()[0]
-	obsbins = lens_selection[binparam].split(',')
-	Nobsbins = len(obsbins)
-
-	# What if there is no observable binning?
-
-	else: # If there is binning...
-		if os.path.isfile(path_obsbins): # from a file
-			Nobsbins = len(np.loadtxt(path_obsbins).T)-1
-		else:
-			try:
-				Nobsbins = int(path_obsbins) # from a specified number (of bins)
-			except:
-				print 'Observable bin file does not exist:', path_obsbins
-				quit()
-		print '%s bins: %s'%(name_obsbins, Nobsbins)
-
-
-	# The number of catalogues that will be ran
-	if ('random' in purpose):
-		Nruns = 0
-	else:
-		Nruns = 1
-
-
-	# Prepare the values for Ncores and Nobsbins
-	Ncores = Ncores/Nobsbins
-	if Ncores == 0:
-		Ncores = 1
-
-
-	# Names of the blind catalogs
-	#blindcats = ['D']
-
-
-    nsplit,blindcat binnum
-
+    return nruns, nsplits, nsplit, nobsbins, nobsbin, blindcat
 
     
-    inputstring = '%s %g %g %g %g %g %g %s %s %i'
-    inputstring += '%s %s %s %s %s %s %g %g %s %s'
-    inputstring = inputstring %inputlist
+def run_shearcodes(purpose, Nruns, Ncore, Ncores, Nobsbins, binnum, blindcats, blindcat):
+    
+    # The shear calculation starts here
 
-    blindcats = ['A', 'B', 'C', 'D']
-    blindcat = blindcats[0]
-    binnum = Nobsbins
-    Ncore = 1
+    # Creating the splits
+    for n in xrange(Nruns):
+        ps = []
+        for binnum in np.arange(Nobsbins)+1:
+            for Ncore in np.arange(Ncores)+1:
+                splitsname = 'python -W ignore shear+covariance.py'
+                splitsname += ' %i %i %i %s &' \
+                            %(Ncore, Ncores, binnum, blindcat)
+                p = sub.Popen(shlex.split(splitsname))
+                ps.append(p)
+        for p in ps:
+            p.wait()
 
-    if purpose == 'binlimits':
-        # Define observable bin limits with equal weight
-        selname = 'python define_obsbins_equal_sn.py %i %i %i %s %s &'\
-                %(Ncore, Ncores, binnum, blindcat, inputstring)
-        p = sub.Popen(shlex.split(selname))
+    quit()
+
+    # Combining the catalog splits to a single output
+    if ('bootstrap' in purpose) or ('catalog' in purpose):
+        runblinds('combine_splits.py', blindcats, Ncore, Ncores, binnum)
+
+    # Stacking the lenses into an ESD profile
+    if ('bootstrap' in purpose) or ('catalog' in purpose):
+        runblinds('stack_shear+bootstrap.py', blindcats, Ncore, Ncores, binnum)        
+
+    # Creating the analytical/bootstrap covariance and ESD profiles
+    if ('bootstrap' in purpose) or ('covariance' in purpose):
+        runblinds('combine_covariance+bootstrap.py', blindcats, Ncore, Ncores, binnum)
+
+    # Plotting the analytical/bootstrap covariance and ESD profiles
+    if ('bootstrap' in purpose) or ('covariance' in purpose):
+        runblinds('plot_covariance+bootstrap.py', blindcats, Ncore, Ncores, binnum)
+
+    return
+    
+    
+def runblinds(codename, blindcats, Ncore, Ncores, binnum):
+    
+    ps = []
+    for blindcat in blindcats:
+        runname = 'python -W ignore %s'%codename
+        runname += ' %i %i %i %s &' \
+                %(Ncore, Ncores, binnum, blindcat)
+        p = sub.Popen(shlex.split(runname))
+        ps.append(p)
+    for p in ps:
         p.wait()
-    else:
-        # The calculation starts here:
-        # Creating the splits
-        for n in xrange(Nruns):
-            ps = []
-            for binnum in np.arange(Nobsbins)+1:
-                for Ncore in np.arange(Ncores)+1:
-                    splitsname = 'python -W ignore shear+covariance.py'
-                    splitsname += '%i %i %i %s %s     &' \
-                                %(Ncore, Ncores, binnum, blindcat, inputstring)
-                    p = sub.Popen(shlex.split(splitsname))
-                    ps.append(p)
-            for p in ps:
-                p.wait()
 
-    # Combining the splits to a single output
-    if ('bootstrap' in purpose) or ('catalog' in purpose):
-
-        ps = []
-        combname = 'python -W ignore combine_splits.py %i %i %i %s %s &' \
-                   %(Ncore, Ncores, binnum, blindcat, inputstring)
-        runcomb =  sub.Popen(shlex.split(combname))
-        ps.append(runcomb)
-        for p in ps:
-            p.wait()
+    return
 
 
-    if ('bootstrap' in purpose) or ('catalog' in purpose):
-        ps = []
-        for blindcat in blindcats:
-            plotname = 'python -W ignore stack_shear+bootstrap.py'
-            plotname += '%i %i %i %s %s     &' \
-                        %(Ncore, Ncores, binnum, blindcat, inputstring)
-            runplot = sub.Popen(shlex.split(plotname))
-            ps.append(runplot)
-        for p in ps:
-            p.wait()
+def main():
+
+    # Importing the input parameters from the config file
+    config_file = '/data2/brouwer/shearprofile/KiDS-GGL/ggl.config'
+
+    # Input for the codes
+    [kids_path, gama_path,
+            Om, Ol, Ok, h,
+            folder, filename, purpose, Rbins, ncores,
+            lensid_file, group_centre, lens_binning, lens_selection,
+            src_selection, blindcats] = esd_utils.read_config(config_file)
+
+    print blindcats
 
 
-    if ('bootstrap' in purpose) or ('covariance' in purpose):
+    print '\n \n \n \n \n \n \n \n \n \n'
+    print 'Running:', purpose
+    print
 
-        ps = []
-        for blindcat in blindcats:
-            combname = 'python -W ignore combine_covariance+bootstrap.py'
-            combname += '%i %i %i %s %s &' \
-                        %(Ncore, Ncores, binnum, blindcat, inputstring)
-            runcomb = sub.Popen(shlex.split(combname))
-            ps.append(runcomb)
-        for p in ps:
-            p.wait()
-
-
-    if ('bootstrap' in purpose) or ('covariance' in purpose):
-        ps = []
-        for blindcat in blindcats:
-            combname = 'python -W ignore plot_covariance+bootstrap.py'
-            combname = '%i %i %i %s %s     &' \
-                       %(Ncore, Ncores, binnum, blindcat, inputstring)
-            runcomb = sub.Popen(shlex.split(combname))
-            ps.append(runcomb)
-        for p in ps:
-            p.wait()
-
+    # Define the initial parameters for this shearcode run
+    nruns, nsplits, nsplit, nobsbins, nobsbin, blindcat = define_runparams(purpose, lens_binning, ncores, blindcats)
+    
+    # Excecute the parallelized shearcode run
+    run_shearcodes(purpose, nruns, nsplits, nsplit, nobsbins, nobsbin, blindcats, blindcat)
 
     end_tot = (time.time()-start_tot)/60
     print 'Finished in: %g minutes' %end_tot
     print
+
     return
+
 
 main()
 

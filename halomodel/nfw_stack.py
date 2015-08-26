8c1dff147fa4644f3e664ada587b5ef9808bf49b
cristobal-sifon <sifon@strw.leidenuniv.nl>
2015-08-26 16:59:05 +0200

    margot's function working

--- halomodel/nfw_stack.py
+++ halomodel/nfw_stack.py
@@ -1,6 +1,6 @@
 from itertools import count, izip
 from numpy import all as npall, array, average, cumsum, digitize
-from numpy import linspace, log, log10, max as npmax, median
+from numpy import linspace, log, log10, max as npmax, median, newaxis
 from numpy import ones, outer, sum as npsum, transpose, zeros
 from numpy.random import random, rayleigh
 from scipy.stats import rv_discrete
@@ -19,18 +19,17 @@ def fiducial(theta, R, h=1, Om=0.315, Ol=0.685):
     _cumsum = cumsum
     _delta = delta
     _izip = izip
-    #_nfw_profile = nfw_profile
     sat_profile, host_profile, central_profile, \
         Rsat, n_Rsat1, n_Rsat2, n_Rsat3, n_Rsat4, \
         fsat, fc_sat, logMsat1, logMsat2, logMsat3, logMsat4, \
         fc_central, logMcentral1, logMcentral2, logMcentral3, logMcentral4, \
         z, Mstar, Rrange, angles = theta
-    n_Rsat = (n_Rsat1, n_Rsat2, n_Rsat3)
+    n_Rsat = (n_Rsat1, n_Rsat2, n_Rsat3, n_Rsat4)
     j = [(ni > 0) for ni in n_Rsat]
     Rsat = [Rsat[i] for i in j]
     n_Rsat = [n[i] for i, n in _izip(j, n_Rsat)]
-    Msat = 10**_array([logMsat1, logMsat2, logMsat3, logMsat4])
-    Mcentral = 10**_array([logMcentral1, logMcentral2,
+    Msat = _array([logMsat1, logMsat2, logMsat3, logMsat4])
+    Mcentral = _array([logMcentral1, logMcentral2,
                            logMcentral3, logMcentral4])
     csat = fc_sat * _cM_duffy08(Msat, z, h)
     ccentral = fc_central * _cM_duffy08(Mcentral, z, h)
@@ -44,23 +43,32 @@ def fiducial(theta, R, h=1, Om=0.315, Ol=0.685):
     #r200_gr = (Mcentral / aux) ** (1./3)
     rs_cent = (Mcentral / aux) ** (1./3) / ccentral
     sigma_central = rs_cent * _delta(ccentral) * rho_m
-    pointmass = Mstar / (3.14159265*(1e6*R[1:])**2)
+    pointmass = [Mi / (3.14159265*(1e6*Ri[1:])**2)
+                 for Mi, Ri in izip(Mstar, R)]
     # satellite signal
-    esd_sat = pointmass + sat_profile(R[1:]/rs_sat, sigma_sat)
+    esd_sat = [pm + sat_profile(Ri[1:]/rs, sigma)
+               for pm, Ri, rs, sigma in izip(pointmass, R, rs_sat, sigma_sat)]
+    esd_sat = _array(esd_sat)
     # satellite host halo signal
-    esd_host = host_profile(R/rs_cent, Rsat/rs_cent, n_Rsat,
-                            sigma_central, Rrange/rs_cent, angles)
+    esd_host = [host_profile(x[0]/x[1], x[2]/x[1], x[3], x[4],
+                             x[5]/x[1], angles)
+                for x in izip(R, rs_cent, Rsat, n_Rsat,
+                              sigma_central, Rrange)]
+    esd_host = _array(esd_host)
     # central signal
-    esd_central = pointmass + central_profile(R[1:]/rs_cent, sigma_central)
+    esd_central = [pm + central_profile(Ri[1:]/rs, sigma)
+                   for pm, Ri, rs, sigma in izip(pointmass, R,
+                                                 rs_cent, sigma_central)]
+    esd_central = _array(esd_central)
     #lnPderived = _log(nfw_profile(rsat, rs_cent, sigma_group)).sum()
     #lnPderived -= _log(_array([romberg(nfw_profile, x[0].min(), rmax,
                                        #args=(x[1],x[2]))
                                #for x in _izip(Rsat, rs_cent,
                                               #sigma_group)])).sum()
-    esd_total = fsat * (esd_sat + esd_host) + (1-fsat) * esd_central
+    esd_total = _array([f * (esat + ehost) + (1-f) * ecentral
+                        for f, esat, ehost, ecentral in izip(fsat, esd_sat,
+                                                             esd_host,
+                                                             esd_central)])
     Mavg = fsat * Msat + (1-fsat) * Mcentral
-    #out = [esd_total, esd_sat, esd_group,
-           #rsat, rt, Msat_rt+Mstar, lnPderived]
-    out = [esd_total, esd_sat, esd_host, esd_central,
-           log10(Mavg), logMsat, logMcentral, 0]
+    out = [esd_total, esd_sat, esd_host, esd_central, Mavg, 0]
     return out

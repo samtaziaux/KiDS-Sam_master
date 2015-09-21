d876d68830d7c211f26e74a0856b289f6d214ef8
cristobal-sifon <sifon@strw.leidenuniv.nl>
2015-09-21 15:20:41 +0200

    added setup_integrand() to sampling_utils

--- sampling/sampling_utils.py
+++ sampling/sampling_utils.py
@@ -177,3 +177,26 @@ def read_function(function):
         for attr in function_path:
             func = getattr(func, attr)
     return func
+
+def setup_integrand(R, k=7):
+    """
+    These are needed for integration and interpolation and should always
+    be used. k=7 gives a precision better than 1% at all radii
+
+    """
+    if R.shape[0] == 1:
+        Rrange = numpy.logspace(numpy.log10(0.99*R.min()),
+                                numpy.log10(1.01*R.max()), 2**k)
+        # this assumes that a value at R=0 will never be provided, which is
+        # obviously true in real observations
+        R = numpy.append(0, R)
+        Rrange = numpy.append(0, Rrange)
+    else:
+        Rrange = [numpy.logspace(numpy.log10(0.99*Ri.min()),
+                                 numpy.log10(1.01*Ri.max()), 2**k)
+                  for Ri in R]
+        R = [numpy.append(0, Ri) for Ri in R]
+        Rrange = [numpy.append(0, Ri) for Ri in Rrange]
+        R = numpy.array(R)
+        Rrange = numpy.array(Rrange)
+    return R, Rrange

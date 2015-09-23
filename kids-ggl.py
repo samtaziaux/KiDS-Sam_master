9ce159cb4a6cfc9f3d527fdcece510c8c7ad9c88
cristobal-sifon <sifon@strw.leidenuniv.nl>
2015-09-23 13:29:37 +0200

    added command-line options to the wrapper

--- kids-ggl.py
+++ kids-ggl.py
@@ -14,21 +14,26 @@ from halomodel import hm_utils
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', dest='config_file')
     parser.add_argument('--demo', dest='demo', action='store_true',
-                        help='do a demo run with the input parameters')
+                        help='do a demo MCMC run with the input parameters')
+    parser.add_argument('--esd', dest='esd', action='store_true',
+                        help='run the ESD production module')
+    parser.add_argument('--sampler', dest='sampler', action='store_true',
+                        help='run the MCMC sampler')
+    parser.add_argument('-c', '--config', dest='config_file')
     args = parser.parse_args()
     # options to turn on and off the data production or halo model?
 
     # ESD data production
-    #shearcode.run_esd(args.config_file)
-    #return
+    if args.esd:
+        shearcode.run_esd(args.config_file)
 
-    # Choose and set up a halo model
-    hm_options = hm_utils.read_config(args.config_file)
-    # Setup and run MCMC sampler
-    sampling_options = sampling_utils.read_config(args.config_file)
-    sampler.run_emcee(hm_options, sampling_options, args)
+    if args.sampler or args.demo:
+        # Choose and set up a halo model
+        hm_options = hm_utils.read_config(args.config_file)
+        # Setup and run MCMC sampler
+        sampling_options = sampling_utils.read_config(args.config_file)
+        sampler.run_emcee(hm_options, sampling_options, args)
 
     return
 

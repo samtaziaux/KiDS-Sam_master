9923193b758a4a5f64cc000f647cb2e7a797052a
andrejdvornik <andrej.dvornik@me.com>
2015-09-25 15:22:04 +0200

    making import halo.py optional

--- halomodel/hm_utils.py
+++ halomodel/hm_utils.py
@@ -7,7 +7,14 @@ from numpy import array, inf, loadtxt
 #sys.path.append('halomodel')
 
 # local
-from halomodel import nfw, nfw_stack, satellites, halo
+from halomodel import nfw, nfw_stack, satellites
+
+# Importing halo.py if and only if all the dependencies exist
+try:
+    from halomodel import halo
+except ImportError:
+    pass
+
 
 def read_config(config_file, version='0.5.7'):
     valid_types = ('normal', 'lognormal', 'uniform', 'exp',

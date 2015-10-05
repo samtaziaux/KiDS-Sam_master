Fixed lensID bug for covariance

esd_production/combine_covariance+bootstrap.py
@@ -224,7 +224,7 @@ def main():
             ESDt_tot[N1], ESDx_tot[N1], error_tot[N1], bias_tot[N1] = shear.calc_stack(gammat[N1], gammax[N1], wk2[N1], np.diagonal(cov[N1,N1,:,:]), srcm[N1], [1,1,1,1], blindcatnum)
             
             # Determine the stacked galIDs
-            binname, lens_binning, Nobsbins, binmin, binmax = shear.define_obsbins(binnum, lens_binning, lenssel_binning, gamacat)
+            binname, lens_binning, Nobsbins, binmin, binmax = shear.define_obsbins(N1, lens_binning, lenssel_binning, gamacat)
             lenssel = shear.define_lenssel(gamacat, centering, lens_selection, lens_binning, binname, binnum, binmin, binmax)
             galIDs = galIDlist[lenssel] # Mask all quantities
             galIDs_matched = galIDs[np.in1d(galIDs, galIDlist_matched)]
@@ -237,7 +237,7 @@ def main():
             for N2 in range(Nobsbins):
                 for R1 in range(nRbins):
                     for R2 in range(nRbins):
-                        cov[N1,N2,R1,R2] = cov[N1,N2,R1,R2]/(wk2[N1,R1]*wk2[N2,R2])/(1e3)**4 # The covariance matrix
+                        cov[N1,N2,R1,R2] = cov[N1,N2,R1,R2]/(wk2[N1,R1]*wk2[N2,R2]) # The covariance matrix
 
 
     with open(filenamecov, 'w') as file:

esd_production/plot_multiple_profiles.py
@@ -29,14 +29,19 @@ ylabel = r'ESD $\langle\Delta\Sigma\rangle$ [h M$_{\odot}/pc^2$]'
 filenames = []
 #filenames = np.append(filenames, [''])
 
+# Redshift test for Koen
 
+
+
+
+"""
 # Pipeline tests
 filenames = np.append(filenames, ['/disks/shear10/brouwer_veersemeer/shearcode_output/output_logmstarbins_oldcatmatch/results_shearcatalog/shearcatalog_rankBCG-999--999_logmstarbin1of8_ZB0.005-1.2_logRbins10:20:2000kpc_Om0.315_h100_oldcatmatch_D.txt'])
 filenames = np.append(filenames, ['/disks/shear10/brouwer_veersemeer/pipeline_testresults/output_logmstarbins_oldcatmatch/results_shearcatalog/shearcatalog_logmstarbin1of8_RankBCGm999_Z_B0p005-1p2_Rbins10-20-2000kpc_Om0p315_Ol0p685_Ok0_h1_oldcatmatch_D.txt'])
 filenames = np.append(filenames, ['/disks/shear10/brouwer_veersemeer/pipeline_testresults/output_logmstarbins_oldcatmatch/results_shearcovariance/shearcovariance_logmstarbin1of8_RankBCGm999_Z_B0p005-1p2_Rbins10-20-2000kpc_Om0p315_Ol0p685_Ok0_h1_oldcatmatch_D.txt'])
 
 
-"""
+
 filenames = np.append(filenames, ['/disks/shear10/brouwer_veersemeer/shearcode_output/output_LumBbins_oldcatmatch/results_shearcatalog/shearcatalog_rankBCG1-1_Nfof5-inf_LumBbin1of6_ZB0.005-1.2_logRbins10:20:2000kpc_Om0.315_h100_oldcatmatch_D.txt'])
 filenames = np.append(filenames, ['/disks/shear10/brouwer_veersemeer/pipeline_testresults/output_LumBbins_oldcatmatch/results_shearcatalog/shearcatalog_LumBbin1of6_Nfof5-inf_RankBCG1_Z_B0p005-1p2_Rbins10-20-2000kpc_Om0p315_Ol0p685_Ok0_h1_oldcatmatch_D.txt'])
 """

esd_production/run_shearcode.py
@@ -12,11 +12,10 @@ def create_config(replacefile, searchlist, replacelist):
     filedata = f.read()
     f.close()
 
-    newdata = filedata
-
     for r in xrange(len(replacelist[0])):
-        
-        fileout = '%s_%s'%(replacefile, replacelist[0, r])
+
+        newdata = filedata
+        fileout = '%s_%s'%(replacefile, r+1)
         
         print
         print 'For %s:'%fileout
@@ -25,11 +24,9 @@ def create_config(replacefile, searchlist, replacelist):
             search = '%s'%searchlist[s]
             replace = '%s'%replacelist[s, r]
             
-            
             print '     - replace %s with %s'%(search, replace)
         
             newdata = newdata.replace(search, replace)
-
         
         f = open(fileout, 'w')
         f.write(newdata)
@@ -49,15 +46,29 @@ replacelist = np.array([np.arange(6)+1, np.array([1724, 2634, 2201, 1577, 3379, 
 config_files = create_config(replacefile, findlist, replacelist)
 """
 
-#config_files = ['/data2/brouwer/shearprofile/KiDS-GGL/configs_margot/ggl_Ned.config']
-config_files = ['/data2/brouwer/shearprofile/KiDS-GGL/configs_margot/ggl_margot.config']
+replacefile = '/data2/brouwer/shearprofile/KiDS-GGL/configs_margot/Edo_logagebins.config'
+findlist = np.array(['agelimit'])
+replacelist = np.array([np.array(['0,9.3', '9.3,9.5', '9.5,inf'])])
+config_files = create_config(replacefile, findlist, replacelist)
+
+
+#config_files = ['/data2/brouwer/shearprofile/KiDS-GGL/configs_margot/ggl_margot.config']
 
 # Edo's age bins
 #config_files = ['/data2/brouwer/shearprofile/KiDS-GGL/configs_margot/Edo_logagebins_%i.config'%(i+1) for i in xrange(3)]
 
+#config_files = ['/data2/brouwer/shearprofile/KiDS-GGL/configs_margot/ggl_margot_mstarweight.config']
 
+"""
+replacefile = '/data2/brouwer/shearprofile/KiDS-GGL/configs_margot/ggl_redshifts.config'
+findlist = np.array(['Zlim'])
+replacelist = np.array([np.array(['inf', '0.13', '0.2', '0.3'])])
+config_files = create_config(replacefile, findlist, replacelist)
+"""
+
+print
 print 'Running:', config_files
+
 
 for config_file in config_files:
     shearcode.run_esd(config_file)
-
1d75dda8b5e175f007acb35dbc543bc7077e88b9
margotbrouwer <margot.brouwer@gmail.com>
2015-10-13 10:57:07 +0200

    Implementing GAMA mask in local density calculation

--- brouwer/environment_project/delta_r_calculator.py
+++ brouwer/environment_project/delta_r_calculator.py
@@ -3,145 +3,151 @@
 "Module to determine the local overdensity delta_r within a sphere of radius r."
 
 # Import the necessary libraries
+import sys
+sys.path.insert(0, '/data2/brouwer/shearprofile/KiDS-GGL/esd_production/')
+
 import pyfits
 import numpy as np
 import distance
-import sys
 import os
 import time
 import shearcode_modules as shear
-from astropy import constants as const, units as u
-import numpy.core._dotblas
-import memory_test as memory
-import time
+import delta_r_utils as utils
 import gc
+from matplotlib import pyplot as plt
 
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+from astropy.coordinates import search_around_sky
 
 # Important constants
-G = const.G.to('pc3/Msun s2') # Gravitational constant
-c = const.c.to('pc/s') # Speed of light
 inf = np.inf # Infinity
 nan = np.nan # Not a number
-pi = np.pi
+pi = np.pi # Pi
 
 purpose = 'shearcatalog'
-centering = 'BCG'
-
+centering = 'None'
 
 O_matter = 0.25
-O_lambda = 1. - O_matter
-h = 1.0
+O_lambda = 1.-O_matter
+Ok = 0.
+h = 1.
 
-galIDlist, groupIDlist, galRAlist, galDEClist, galZlist, Dcllist, Dallist, Nfoflist, galranklist, petrolist, nQlist = shear.import_gamacat(centering, purpose, 1, -999, inf, O_matter, O_lambda, h, 'Rpetro2', 'nQ')
-galIDlist, groupIDlist, galRAlist, galDEClist, galZlist, Dcllist, Dallist, Nfoflist, galranklist, delta8list, complist = shear.import_gamacat(centering, purpose, 1, -999, inf, O_matter, O_lambda, h, 'delta8', 'Completeness')
-galIDlist, groupIDlist, galRAlist, galDEClist, galZlist, Dcllist, Dallist, Nfoflist, galranklist, gmaglist, rmaglist = shear.import_gamacat(centering, purpose, 1, -999, inf, O_matter, O_lambda, h, 'absmag_g', 'absmag_r')
+
+
+# Importing the GAMA catalogue
+path_gamacat = '/data2/brouwer/MergedCatalogues/GAMACatalogue_1.0.fits'
+centering = 'None'
+purpose = 'shearcatalog'
+Ncat = 1
+Runit = 'kpc'
+lens_weights = {'None': ''}
+
+gamacat, galIDlist, galRAlist, galDEClist, galweightlist, galZlist, Dcllist, Dallist = \
+shear.import_gamacat(path_gamacat, centering, purpose, Ncat, \
+O_matter, O_lambda, Ok, h, Runit, lens_weights)
+
+# Conversion from pc to Mpc
+Dcllist = Dcllist/1e6
+Dallist = Dallist/1e6
+
+petrolist = gamacat['Rpetro2'] # petrosian magnitude of all galaxies
+nQlist = gamacat['nQ'] # nQ of all galaxies
+delta8list = gamacat['delta8']# delta8 of all galaxies
+complist = gamacat['Completeness'] # Completeness around all galaxies
+gmaglist = gamacat['absmag_g'] # g magnitude of all galaxies
+rmaglist = gamacat['absmag_r'] # r magnitude of all galaxies
 
 # Defining the color (g-r)0
 colorlist = gmaglist-rmaglist
 
-# Conversion from kpc to Mpc
-Dcllist = Dcllist/1e3
-Dallist = Dallist/1e3
-
-
-# Calculation the k(z)-correction
-colorbins = [-1.344980, 0.2, 0.34, 0.48, 0.62, 0.76, 0.9, 16.99882]
-Ncolorbins = len(colorbins)-1
-kcor_table = np.array([[-31.36066, 38.63440, -14.79088, 1.427126, 1.3007671E-03], [-17.76654, 25.49545, -10.79095, 1.365723, 6.2354435E-03], [-12.94193, 21.43714, -9.825956, 1.683059, -1.9719140E-03], [-6.399204, 14.76284, -7.473374, 1.847070, -6.8006814E-03], [9.017070, -1.390476, -0.9145135, 1.375556, -4.7244146E-03], [14.78212, -6.591838, 0.9443237, 1.357406, -5.1314551E-03], [15.09393, -5.729835, -0.2097371, 1.859484, -1.2495981E-02]])
-
-klist = np.zeros(len(galIDlist))
-for c in xrange(Ncolorbins):
-	
-	colormask = (colorbins[c]<colorlist)&(colorlist<colorbins[c+1])
-
-	acol = kcor_table[c]
-	for a in xrange(len(acol)):
-		klist[colormask] = klist[colormask] + acol[a]*galZlist[colormask]**(4-a)
-
-#	print c+1
-#	print np.median(colorlist[colormask])
-#	print
-
-# Calculating the E(z)-correction
-Q0all = 0.97
-Q0list = -Q0all*galZlist
-
-print 'petromax', np.amax(petrolist[np.isfinite(petrolist)])
-
-# Defining the magnitude
-maglist = petrolist - 5*np.log10((1+galZlist)*Dcllist) - 25. - klist - Q0list
+maglist = utils.calc_magnitude(galZlist, Dcllist, colorlist, petrolist)
 
 # Masking of unused lenses
-Rmax = np.arange(1,9)
 zmin = 0.039
 zmax = 0.263
 
 lensmask = (zmin<galZlist)&(galZlist<zmax)&(nQlist>=3)&(complist>0.8)#&(np.isfinite(complist))
 DDPmask = (lensmask)&(-21.8<maglist)&(maglist<-20.1)
 
-galIDs, galRAs, galDECs, galZs, Dcls, Dals, galIDmask = shear.mask_gamacat(purpose, galIDlist, lensmask, centering, galranklist, galIDlist, galRAlist, galDEClist, galZlist, Dcllist, Dallist) # Mask all GAMA galaxies that are not in this field
-DDP_IDs, DDP_RAs, DDP_DECs, DDP_Zs, DDP_Dcls, DDP_Dals, DDP_IDmask = shear.mask_gamacat(purpose, galIDlist, DDPmask, centering, galranklist, galIDlist, galRAlist, galDEClist, galZlist, Dcllist, Dallist) # Mask all GAMA galaxies that are not in this field
+galIDs, galRAs, galDECs, galZs, Dcls, Dals, delta8s, comps = [lenslist[lensmask] \
+for lenslist in [galIDlist, galRAlist, galDEClist, galZlist, Dcllist, Dallist, delta8list, complist]] # Mask all non-selected GAMA galaxies
 
-delta8 = delta8list[galIDmask]
-comps = complist[galIDmask]
+DDP_IDs, DDP_RAs, DDP_DECs, DDP_Zs, DDP_Dcls, DDP_Dals = [lenslist[DDPmask] \
+for lenslist in [galIDlist, galRAlist, galDEClist, galZlist, Dcllist, Dallist]] # Mask all non-DDP GAMA galaxies
 
-rholist = np.zeros([len(galIDs), 8])
+galcoords =  SkyCoord(ra=galRAs*u.degree, dec=galDECs*u.degree, frame='icrs')
 
-print len(galIDlist)
-print len(galIDs)
-print len(DDP_IDs)
-print 'Rmax:', Rmax
+print
+print 'Number of galaxies:'
+print 'All (no cut):', len(galIDlist)
+print 'Used lenses:', len(galIDs)
+print 'Density Defining Population:', len(DDP_IDs)
+print
 
-# Calculating the volume and mean density of GAMA
-"""
-gama1 = [[129., 141.],[-2.,3.]]
-gama2 = [[174., 186.],[-3.,2.]]
-gama3 = [[211.5, 223.5],[-2.,3.]]
 
-r1 = distance.comoving(0.039, O_matter, O_lambda, h)/1e3
-r2 = distance.comoving(0.263, O_matter, O_lambda, h)/1e3
 
-gamacoords = np.array([gama1,gama2,gama3])
+# Import GAMA masks for completeness calculation
+path_gamamasks = ['/disks/shear10/GamaMasks/%smask08000.fits'%g for g in ['g09', 'g12', 'g15']]
+gamamasks = np.array([pyfits.open(path_gamamask, ignore_missing_end=True)['PRIMARY'].data for path_gamamask in path_gamamasks])
+gamamasks[gamamasks ==-2.] = 0.
+print gamamasks
 
-gamavol = 0.
-for i in xrange(len(gamacoords)):
-    theta1 = np.radians(-gamacoords[i,0,0])
-    theta2 = np.radians(-gamacoords[i,0,1])
-    phi1 = np.radians(90.-gamacoords[i,1,0])
-    phi2 = np.radians(90.-gamacoords[i,1,1])
+# Creating the RA and DEC coordinates of each GAMA field
+pixsize = 0.1
+
+gama1 = [[129., 141.], [-2.,3.]]
+gama2 = [[174., 186.], [-3.,2.]]
+gama3 = [[211.5, 223.5], [-2.,3.]]
+
+gamalims = np.array([gama1, gama2, gama3])
+
+# Matching the GAMA coordinates to the lenses within Rmax
+for f in xrange(len(gamalims)): # For each GAMA field ...
     
-    gamavol = gamavol + 1./3. * (theta1 - theta2) * (r1**3 - r2**3) * (np.cos(phi1) - np.cos(phi2))
-"""
-gamavol = 6.75e6
+    print 'GAMA field %i:'%(f+1)
+    
+    gamamask = gamamasks[f]
+    gamacoords = utils.create_fieldcoords(gamalims, pixsize, f)
 
-print 'GAMA volume:', gamavol, '(Mpc/h)^3'
+    Ngals = 10
+    for g in xrange(Ngals):
 
+        print float(g+1)/float(Ngals)*100., '%'
+        Rmax = 1*u.deg
+
+        idxlenses, idxgama, sep2d, dist3d = search_around_sky(galcoords, gamacoords, Rmax)
+
+        plt.plot(idxlenses)
+        plt.plot(gamaRAs, gamaDECs, '.')
+        plt.show()
+
+        #print np.amax(idxlenses), np.amax(idxgama), np.amax(sep2d), np.amax(dist3d)
+
+quit()
+
+
+# Calculating the volume and mean DDP density of GAMA
+gamavol = utils.calc_rho_mean(gamalims, O_matter, O_lambda, h)
 rho_mean = len(DDP_IDs)/gamavol
 print 'mean density', rho_mean
 
-def calc_rho(Rmax, galRA, DDP_RAs, galDEC, DDP_DECs, Dcl, DDP_Dcls, comp):
 
-    rho_r = np.array([])
-    
-    # Calculate the distance between two galaxies (in kpc)
-    theta = (np.arccos(np.cos(np.radians(DDP_DECs))*np.cos(np.radians(galDEC))*np.cos(np.radians(DDP_RAs-galRA)) + np.sin(np.radians(DDP_DECs))*np.sin(np.radians(galDEC))))
-    galdist = (Dcl**2 + DDP_Dcls**2 - 2*Dcl*DDP_Dcls*np.cos(theta))**0.5
-    
-    galdist = galdist[np.isfinite(galdist)]
-    
-    for R in Rmax:
-        rhomask = galdist<R
-        result = sum(rhomask)/(4./3.*pi*R**3)/comp
-        rho_r = np.append(rho_r, result)      
+# Calculating the number density rho around each lens, for different sphere sizes
+Rmax = np.arange(1,9) # Maximum radius around each lens
+print 'Rmax:', Rmax
 
-    return rho_r
+# For each Rmax, this list will contain the number density rho around each lens
+rholist = np.zeros([len(galIDs), len(Rmax)])
 
-
-Ngals=len(galIDs)
+# For each lens ...
+#Ngals=len(galIDs)
+Ngals = 10
 for g in xrange(Ngals):
 
-    print float(g)/float(Ngals)*100., '%'
+    print float(g+1)/float(Ngals)*100., '%'
     
+    # ... select all properties of that lens
     galID = galIDs[g]
     galRA = galRAs[g]
     galDEC = galDECs[g]
@@ -150,22 +156,23 @@ for g in xrange(Ngals):
     Dal = Dals[g]
     comp = comps[g]
     
-    rho_r = calc_rho(Rmax, galRA, DDP_RAs, galDEC, DDP_DECs, Dcl, DDP_Dcls, comp)
+    # ... calculate rho around this lens
+    rho_r = utils.calc_rho(Rmax, galRA, DDP_RAs, galDEC, DDP_DECs, Dcl, DDP_Dcls, comp)
 
     rholist[g] = rho_r
 
+# Calculate the density delta for each lens and Rmax
 delta_r = (rholist-rho_mean)/rho_mean
     
-print 'mean density', rho_mean
-print
 
-print 'density rho', rholist
+print 'Density rho:', rholist
 print
-
+print 'Mean density:', rho_mean
+print
 print 'delta_r (Margot):', delta_r[:,7][0:Ngals]
-print 'delta_r (Tamsyn):', delta8[0:Ngals]
+print 'delta_r (Tamsyn):', delta8s[0:Ngals]
 
-print 'difference', np.sum((delta_r[:,7][0:Ngals]-delta8[0:Ngals])**2)
+print 'difference', np.sum((delta_r[:,7][0:Ngals]-delta8s[0:Ngals])**2)
 
 
 #Ngal = 88.1e3
@@ -176,7 +183,7 @@ print 'difference', np.sum((delta_r[:,7][0:Ngals]-delta8[0:Ngals])**2)
 
 filename = 'delta_r_new.txt'
 with open(filename, 'w') as file:
-	print >>file, '# galID     delta(R=1Mpc)     delta(R=2Mpc)     delta(R=3Mpc)     delta(R=4Mpc)     delta(R=5Mpc)     delta(R=6Mpc)     delta(R=7Mpc)     delta(R=8Mpc)'
+    print >>file, '# galID     delta(R=1Mpc)     delta(R=2Mpc)     delta(R=3Mpc)     delta(R=4Mpc)     delta(R=5Mpc)     delta(R=6Mpc)     delta(R=7Mpc)     delta(R=8Mpc)'
 
 with open(filename, 'a') as file:
     for g in xrange(len(galIDs)):
---
+++ brouwer/environment_project/delta_r_utils.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python
+
+"Module to determine the local overdensity delta_r within a sphere of radius r."
+
+# Import the necessary libraries
+import sys
+sys.path.insert(0, '/data2/brouwer/shearprofile/KiDS-GGL/esd_production/')
+
+import pyfits
+import numpy as np
+import distance
+import os
+import time
+import shearcode_modules as shear
+import gc
+from matplotlib import pyplot as plt
+
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+from astropy.coordinates import search_around_sky
+
+pi = np.pi
+
+
+# Creating the RA and DEC coordinates of each GAMA field
+def create_fieldcoords(gamalims, pixsize, field):
+    # Creating all coordinates within this GAMA field
+
+    gamaRAlist = []
+    gamaDEClist = []
+
+    gamaRAlist = np.arange(gamalims[field, 0, 0], gamalims[field, 0, 1], pixsize)+pixsize/2
+    gamaDEClist = np.arange(gamalims[field, 1, 0], gamalims[field, 1, 1], pixsize)+pixsize/2
+
+    gamaRAs, gamaDECs = np.meshgrid(gamaRAlist, gamaDEClist)
+
+    gamaRAs = np.reshape(gamaRAs, [np.size(gamaRAs)])
+    gamaDECs = np.reshape(gamaDECs, [np.size(gamaDECs)])
+
+    gamacoords = SkyCoord(ra=gamaRAs*u.degree, dec=gamaDECs*u.degree, frame='icrs')
+    
+    return gamacoords
+    
+
+# Calculating the galaxy magnitudes (following McNaught-Roberts et al.)
+def calc_magnitude(galZlist, Dcllist, colorlist, petrolist):
+    
+    # Calculation the k(z)-correction
+    colorbins = [-1.344980, 0.2, 0.34, 0.48, 0.62, 0.76, 0.9, 16.99882]
+    Ncolorbins = len(colorbins)-1
+    kcor_table = \
+    np.array([[-31.36066, 38.63440, -14.79088, 1.427126, 1.3007671E-03], \
+    [-17.76654, 25.49545, -10.79095, 1.365723, 6.2354435E-03], \
+    [-12.94193, 21.43714, -9.825956, 1.683059, -1.9719140E-03], \
+    [-6.399204, 14.76284, -7.473374, 1.847070, -6.8006814E-03], \
+    [9.017070, -1.390476, -0.9145135, 1.375556, -4.7244146E-03], \
+    [14.78212, -6.591838, 0.9443237, 1.357406, -5.1314551E-03], \
+    [15.09393, -5.729835, -0.2097371, 1.859484, -1.2495981E-02]])
+
+    klist = np.zeros(len(galZlist))
+    for c in xrange(Ncolorbins):
+        
+        colormask = (colorbins[c]<colorlist)&(colorlist<colorbins[c+1])
+
+        acol = kcor_table[c]
+        for a in xrange(len(acol)):
+            klist[colormask] = klist[colormask] + acol[a]*galZlist[colormask]**(4-a)
+
+    # Calculating the E(z)-correction
+    Q0all = 0.97
+    Q0list = -Q0all*galZlist
+
+    # Defining the magnitude
+    maglist = petrolist - 5*np.log10( (1+galZlist) * (Dcllist.to(u.Mpc)).value ) - 25. - klist - Q0list
+    
+    return maglist
+
+
+# Calculate the number density rho around one lens
+def calc_rho(Rmax, galRA, DDP_RAs, galDEC, DDP_DECs, Dcl, DDP_Dcls, comp):
+
+    rho_r = np.array([])
+    
+    # Calculate the distance between two galaxies (in Mpc)
+    theta = (np.arccos(np.cos(np.radians(DDP_DECs))*np.cos(np.radians(galDEC))*np.cos(np.radians(DDP_RAs-galRA)) + np.sin(np.radians(DDP_DECs))*np.sin(np.radians(galDEC))))
+    galdist = (Dcl**2 + DDP_Dcls**2 - 2*Dcl*DDP_Dcls*np.cos(theta))**0.5
+    galdist = galdist[np.isfinite(galdist)]
+    
+    for R in Rmax:
+        rhomask = galdist<R
+        result = sum(rhomask)/(4./3.*pi*R**3)/comp
+        rho_r = np.append(rho_r, result)
+
+    return rho_r
+
+
+# Calculating the volume of GAMA
+def calc_rho_mean(gamalims, O_matter, O_lambda, h):
+    
+    """
+    r1 = distance.comoving(0.039, O_matter, O_lambda, h)/1e3
+    r2 = distance.comoving(0.263, O_matter, O_lambda, h)/1e3
+
+    gamacoords = np.array([gama1,gama2,gama3])
+
+    gamavol = 0.
+    for i in xrange(len(gamacoords)):
+        theta1 = np.radians(-gamalims[i,0,0])
+        theta2 = np.radians(-gamalims[i,0,1])
+        phi1 = np.radians(90.-gamalims[i,1,0])
+        phi2 = np.radians(90.-gamalims[i,1,1])
+        
+        gamavol = gamavol + 1./3. * (theta1 - theta2) * (r1**3 - r2**3) * (np.cos(phi1) - np.cos(phi2))
+    """
+    
+    gamavol = 6.75e6
+    print 'GAMA volume:', gamavol, '(Mpc/h)^3'
+
+    return gamavol

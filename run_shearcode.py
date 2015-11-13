a02593eda057a96b2620c605daebcf272931e3fb
margotbrouwer <margot.brouwer@gmail.com>
2015-11-13 16:04:16 +0100

    Setting up new cen+iso environment halo model fit

--- run_shearcode.py
+++ run_shearcode.py
@@ -72,12 +72,14 @@ config_files = create_config(replacefile, findlist, replacelist)
 
 # Edo's age bins
 #config_files = ['brouwer/configs_margot/Edo_logagebins_%i.config'%(i+1) for i in xrange(3)]
-config_files = ['brouwer/configs_margot/Edo_logagebins.config']
+#config_files = ['brouwer/configs_margot/Edo_logagebins.config']
 
 #config_files = ['brouwer/configs_margot/ggl_environments_all.config']
 #config_files = ['brouwer/configs_margot/ggl_environments_cen+iso.config']
 
 #config_files = ['brouwer/configs_margot/troughs.config']
+
+config_files = ['brouwer/configs_margot/ggl_environments_%s.config'%x for x in ['all', 'cen+iso', 'all_shuffled', 'cen+iso_shuffled']]
 
 
 print

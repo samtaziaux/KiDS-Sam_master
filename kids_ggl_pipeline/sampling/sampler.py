9788a542386499801642fec61e1f8f90c926d9f9
Cristóbal Sifón <sifon@strw.leidenuniv.nl>
2016-02-26 00:33:40 +0100

    added small offset to parameters starting at zero

--- kids_ggl_pipeline/sampling/sampler.py
+++ kids_ggl_pipeline/sampling/sampler.py
@@ -75,6 +75,7 @@ def run_emcee(hm_options, sampling_options, args):
         msg = 'ERROR: Not all starting points defined for free parameters.'
         print msg
         exit()
+    starting[starting == 0] = 1e-3
     print 'starting values =', starting
 
     # identify the function. Raises an AttributeError if not found
@@ -221,6 +222,7 @@ def run_emcee(hm_options, sampling_options, args):
     for i, result in enumerate(sampler.sample(pos, iterations=nsteps,
                                               thin=thin)):
         # make sure that nwalkers is a factor of this number!
+        print i, '\n'
         if i*nwalkers % update_freq == nwalkers:
             out = write_to_fits(output, chi2, sampler, nwalkers, thin,
                                 params, jfree, metadata, meta_names, i,

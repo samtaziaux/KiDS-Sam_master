d9485dc5a3d87407297d8466d45ffdb5ecf4c514
cristobal-sifon <sifon@astro.princeton.edu>
2017-07-28 17:45:33 -0400

    further py3 compatibility

--- kids_ggl_pipeline/halomodel/nfw.py
+++ kids_ggl_pipeline/halomodel/nfw.py
@@ -67,7 +67,8 @@ Notes
     -Remember that the "sharp" profile is only defined up to R=r_t!
 
 """
-from __future__ import division
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 
 try:
     from itertools import izip
@@ -83,6 +84,7 @@ from scipy.interpolate import interp1d
 #--    Excess surface densities
 #-----------------------------------#
 
+
 def esd(x, sigma_s):
     """
     Regular NFW.
@@ -92,6 +94,7 @@ def esd(x, sigma_s):
 
     """
     return barsigma(x, sigma_s) - sigma(x, sigma_s)
+
 
 def esd_trunc5(x, tau, sigma_s):
     """
@@ -104,6 +107,7 @@ def esd_trunc5(x, tau, sigma_s):
     """
     return barsigma_trunc5(x, tau, sigma_s) - sigma_trunc5(x, tau, sigma_s)
 
+
 def esd_trunc7(x, tau, sigma_s):
     """
     NFW that drops as 1/r^7 at large radius.
@@ -113,6 +117,7 @@ def esd_trunc7(x, tau, sigma_s):
 
     """
     return barsigma_trunc7(x, tau, sigma_s) - sigma_trunc7(x, tau, sigma_s)
+
 
 def esd_sharp(x, tau, sigma_s):
     """
@@ -134,6 +139,7 @@ Will later try to
 
 """
 
+
 def esd_offset(x, xoff, n, sigma_s, x_range, angles, interp_kind='slinear'):
     """
     Remember that the first value of x *must* be zero for cumtrapz to
@@ -149,9 +155,11 @@ def esd_offset(x, xoff, n, sigma_s, x_range, angles, interp_kind='slinear'):
     s_within_x = cumtrapz(sigma_within(s, x), x, initial=0)
     return 2.0 * array(s_within_x)[1:] / x[1:]**2.0 - s_at_x
 
+
 def sigma_azimuthal(angles, x_range, xoff, sigma_s):
     xo = (xoff**2.0 + x_range**2.0 + 2.0*x_range*xoff*cos(angles)) ** 0.5
     return sigma(xo, sigma_s)
+
 
 def sigma_distribution(xoff, n, sigma_s, x_range, angles,
                        interp_kind='slinear'):
@@ -163,6 +171,7 @@ def sigma_distribution(xoff, n, sigma_s, x_range, angles,
     s = interp1d(x_range, s, kind=interp_kind)
     return s
 
+
 def sigma_integrate(x_range, xoff, sigma_s, angles):
     x_range2d, angles2d = meshgrid(x_range, angles)
     y = [sigma_azimuthal(angles2d, x_range2d, x, sigma_s) for x in xoff]
@@ -171,16 +180,20 @@ def sigma_integrate(x_range, xoff, sigma_s, angles):
     s = [interp1d(x_range, i, kind='slinear') for i in itable]
     return s
 
+
 def sigma_offset(x, xoff, n, sigma_s, x_range, angles, interp_kind='slinear'):
     s = sigma_distribution(xoff, n, sigma_s, x_range, angles, interp_kind)
     return s(x[1:]) / (2.0*3.14159265)
 
+
 def sigma_within(s, x):
     return s(x) * x / (2.0*3.14159265)
+
 
 #-----------------------------------#
 #--    Surface densities
 #-----------------------------------#
+
 
 def sigma(x, sigma_s):
     s = ones(x.shape)
@@ -194,11 +207,13 @@ def sigma(x, sigma_s):
     s[j] = (1 - 2.*s[j] / (x[j]**2 - 1)**0.5) / (x[j]**2 - 1.)
     return 2 * sigma_s * s / 1e12
 
+
 def sigma_trunc5(x, tau, sigma_s):
     s = (tau**2 + 1) / (x**2 - 1) * (1 - F(x))
     s += 2*F(x) - 3.14159265 / (tau**2 + x**2)**0.5
     s += (tau**2 - 1) / (tau * (tau**2 + x**2)**0.5) * L(x, tau)
     return 2 * sigma_s * tau**2 / (tau**2 + 1)**2 * s / 1e12
+
 
 def sigma_trunc7(x, tau, sigma_s):
     xtau2 = tau**2 + x**2
@@ -209,6 +224,7 @@ def sigma_trunc7(x, tau, sigma_s):
          L(x, tau) / (tau**3 * xtau2**1.5)
     return tau**4 * sigma_s / (tau**2 + 1)**3 * s / 1e12
 
+
 def sigma_sharp(x, tau, sigma_s):
     mask = x < tau
     xmask = x[mask]
@@ -216,9 +232,11 @@ def sigma_sharp(x, tau, sigma_s):
     s[mask] = (tau**2-xmask**2)**0.5 / (1.+tau) - T(xmask, tau)
     return 2 * sigma_s / (x**2 - 1) * s.real / 1e12
 
+
 #-----------------------------------#
 #--    Enclosed surface densities
 #-----------------------------------#
+
 
 def barsigma(x, sigma_s):
     s = ones(x.shape)
@@ -237,12 +255,14 @@ def barsigma(x, sigma_s):
     s[j] = (s[j] + log(0.5*x[j])) / x[j]**2
     return 4 * sigma_s * s / 1e12
 
+
 def barsigma_trunc5(x, tau, sigma_s):
     s = hypot(tau, x) * (-3.14159265 + (tau**2 - 1) / tau * L(x, tau))
     s += tau * 3.14159265 + (tau**2 - 1) * log(tau)
     s += (tau**2 + 1 + 2 * (x**2 - 1)) * F(x)
     s *= (4 * sigma_s / x**2) * (tau**2 / (tau**2 + 1)**2)
     return s / 1e12
+
 
 def barsigma_trunc7(x, tau, sigma_s):
     xtau2 = tau**2 + x**2
@@ -256,6 +276,7 @@ def barsigma_trunc7(x, tau, sigma_s):
     s = 2 * sigma_s * tau**4 / (x**2 * (tau**2 + 1)**3) * (s1 + s2)
     return s / 1e12
 
+
 def barsigma_sharp(x, tau, sigma_s):
     x = x + 0j
     s = log(1.+tau) - (tau - (tau**2-x**2)**0.5) / (1.+tau)
@@ -263,9 +284,11 @@ def barsigma_sharp(x, tau, sigma_s):
     s *= 4 * sigma_s / x**2
     return s.real / 1e12
 
+
 #-----------------------------------#
 #--    Enclosed projected masses
 #-----------------------------------#
+
 
 def mproj_enclosed(x, rs, sigma_s):
     """
@@ -276,6 +299,7 @@ def mproj_enclosed(x, rs, sigma_s):
 
     """
     return 2*3.14159265 * (rs*x)**2 * (1e12 * barsigma(x, sigma_s))
+
 
 def mproj_enclosed_trunc5(x, rs, tau, sigma_s):
     """
@@ -288,6 +312,7 @@ def mproj_enclosed_trunc5(x, rs, tau, sigma_s):
     """
     return 2*3.14159265 * (rs*x)**2 * (1e12 * barsigma_trunc5(x, tau, sigma_s))
 
+
 def mproj_enclosed_trunc7(x, rs, tau, sigma_s):
     """
     NFW that drops as 1/r^7 at large radius.
@@ -297,6 +322,7 @@ def mproj_enclosed_trunc7(x, rs, tau, sigma_s):
 
     """
     return 2*3.14159265 * (rs*x)**2 * (1e12 * barsigma_trunc7(x, tau, sigma_s))
+
 
 def mproj_enclosed_sharp(x, rs, tau, sigma_s):
     """
@@ -308,9 +334,11 @@ def mproj_enclosed_sharp(x, rs, tau, sigma_s):
     """
     return 2*3.14159265 * (rs*x)**2 * (1e12 * barsigma_sharp(x, tau, sigma_s))
 
+
 #-----------------------------------#
 #--    Enclosed 3D masses
 #-----------------------------------#
+
 
 def mass_enclosed(x, rs, sigma_s):
     """
@@ -321,6 +349,7 @@ def mass_enclosed(x, rs, sigma_s):
 
     """
     return 4 * 3.14159265 * rs**2 * sigma_s * (log(1.+x) - x / (1.+x))
+
 
 def mass_enclosed_trunc5(x, rs, tau, sigma_s):
     """
@@ -335,6 +364,7 @@ def mass_enclosed_trunc5(x, rs, tau, sigma_s):
                    2 * (-1+tau**2) * log(1.+X) - \
                    (-1 + tau**2) * log(tau**2+X**2)) / (2 * (1.+tau**2)**2)
     return 4 * 3.14159265 * rs**2 * sigma_s * tau**2 * (f(x) - f(0))
+
 
 def mass_enclosed_trunc7(x, rs, tau, sigma_s):
     """
@@ -352,6 +382,7 @@ def mass_enclosed_trunc7(x, rs, tau, sigma_s):
                   (2 * (tau**2 + 1)**3)
     return 4 * 3.14159265 * rs**2 * sigma_s * tau**4 * (f(x) - f(0))
 
+
 def mass_enclosed_sharp(x, rs, tau, sigma_s):
     """
     NFW sharply truncated at the truncation radius.
@@ -362,12 +393,15 @@ def mass_enclosed_sharp(x, rs, tau, sigma_s):
     """
     return 4 * 3.14159265 * rs**2 * sigma_s * (log(1.+x) - x / (1.+x))
 
+
 #-----------------------------------#
 #--    Total  masses
 #-----------------------------------#
 
+
 def mass_total(rs, c, sigma_s):
     return 4 * 3.14159265 * rs**2 * sigma_s * (log(1.+c) - c / (1.+c))
+
 
 def mass_total_trunc5(rs, tau, sigma_s):
     """
@@ -381,6 +415,7 @@ def mass_total_trunc5(rs, tau, sigma_s):
     m = 4 * 3.14159265 * rs**2 * sigma_s * tau**2 / (tau**2 + 1)**2
     return m * ((tau**2 - 1) * log(tau) + tau*3.14159265 - (tau**2 + 1))
 
+
 def mass_total_trunc7(rs, tau, sigma_s):
     """
     NFW that drops as 1/r^7 at large radius.
@@ -392,6 +427,7 @@ def mass_total_trunc7(rs, tau, sigma_s):
     m = 4 * 3.14159265358 * rs**2 * sigma_s * tau**2 / (2 * (tau**2 + 1)**3)
     return m * (2 * tau**2 * (tau**2 - 3) * log(tau) - \
                 (3*tau**2 - 1) * (tau**2 + 1 - tau*3.14159265))
+
 
 def mass_total_sharp(rs, tau, sigma_s):
     """
@@ -407,23 +443,29 @@ def mass_total_sharp(rs, tau, sigma_s):
 #--    3-dimensional density profiles
 #-----------------------------------#
 
+
 def rho(x, delta_c, rho_bg):
     return delta_c * rho_bg / (x*(1+x)**2)
+
 
 def rho_trunc5(x, tau, delta_c, rho_bg):
     return delta_c * rho_bg / (x*(1+x)**2) * (tau**2 / (tau**2+x**2))
 
+
 def rho_trunc7(x, tau, delta_c, rho_bg):
     return delta_c * rho_bg / (x*(1+x)**2) * (tau**2 / (tau**2+x**2))**2
+
 
 def rho_sharp(x, tau, delta_c, rho_bg):
     y = zeros(x.size)
     y[x < tau] = delta_c * rho_bg / (x[x < tau]*(1+x[x < tau])**2)
     return y
 
+
 #-----------------------------------#
 #--    Auxiliary functions
 #-----------------------------------#
+
 
 def F(x):
     x = array(x)
@@ -433,8 +475,10 @@ def F(x):
     f[x > 1] = arccos(1./x[x > 1]) / (x[x > 1]**2 - 1)**0.5
     return f
 
+
 def L(x, tau):
     return log(x / ((tau**2+x**2)**0.5 + tau))
+
 
 def T(x, tau):
     x = x + 0j
@@ -446,8 +490,10 @@ def T(x, tau):
     # the imaginary parts of the two arctans cancel out
     return t
 
+
 def Sigma_s(rs, delta_c, rho_c):
     return rs * delta_c * rho_c
+
 
 def delta(c, overdensity=200.):
     return overdensity * c**3 / (3 * (log(1.+c) - c / (1.+c)))

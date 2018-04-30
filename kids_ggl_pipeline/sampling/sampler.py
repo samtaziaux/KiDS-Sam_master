d082362822930d3f3fbbddf4ae8182f21aa17a03
Cristobal Sifon <sifon@astro.princeton.edu>
2018-04-30 08:44:12 -0300

    created write_hdr and run_demo for tidiness

--- kids_ggl_pipeline/sampling/sampler.py
+++ kids_ggl_pipeline/sampling/sampler.py
@@ -12,6 +12,7 @@ import os
 import sys
 from astropy.io.fits import BinTableHDU, Column, Header, PrimaryHDU
 from itertools import count
+from matplotlib import cm, pylab
 from numpy import all as npall, array, append, concatenate, dot, inf, isnan, \
                   isfinite, log, log10, outer, pi, sqrt, squeeze, transpose, \
                   zeros
@@ -88,127 +89,16 @@ def run_emcee(hm_options, sampling_options, args):
     starting[starting == 0] = 1e-3
     print('starting values =', starting)
 
-    # identify the function. Raises an AttributeError if not found
-    #function = model.model()
-    #sat_profile = params.sat_profile()
-    #group_profile = params.group_profile()
-    #function = model
-
-    if not args.demo:
-        hdrfile = '.'.join(output.split('.')[:-1]) + '.hdr'
-        print('Printing header information to', hdrfile)
-        hdr = open(hdrfile, 'w')
-        print('Started', ctime(), file=hdr)
-        print('datafile', ','.join(datafile), file=hdr)
-        print('cols', ','.join([str(c) for c in datacols]), file=hdr)
-        print('covfile', covfile, file=hdr)
-        print('covcols', ','.join([str(c) for c in covcols]), file=hdr)
-        if exclude_bins is not None:
-            print('exclude_bins', ','.join([str(c) for c in exclude_bins]),
-                  file=hdr)
-        print('model {0}'.format(function), file=hdr)
-        for p, pt, v1, v2, v3, v4 in izip(params, prior_types,
-                                          val1, val2, val3, val4):
-            try:
-                line = '%s  %s  ' %(p, pt)
-                line += ','.join(numpy.array(v1, dtype=str))
-            except TypeError:
-                line = '%s  %s  %s' %(p, pt, str(v1))
-                if pt not in ('fixed', 'function', 'read'):
-                    # I don't know why v2, v3 and v4 are single-element
-                    # arrays instead of floats but who cares if all the rest
-                    # works... I'm leaving the try statement just in case they
-                    # might be floats at some point
-                    try:
-                        line += '  %s  %s  %s' \
-                                %tuple([str(v[0]) for v in (v2,v3,v4)])
-                    except TypeError:
-                        line += '  %s  %s  %s' \
-                                %tuple([str(v) for v in (v2,v3,v4)])
-            print(line, file=hdr)
-        print('nwalkers  {0:5d}'.format(nwalkers), file=hdr)
-        print('nsteps    {0:5d}'.format(nsteps), file=hdr)
-        print('nburn     {0:5d}'.format(nburn), file=hdr)
-        print('thin      {0:5d}'.format(thin), file=hdr)
-        hdr.close()
-
-
     # are we just running a demo?
     if args.demo:
-        from matplotlib import cm
-        import matplotlib.pylab as pylab
-        def plot_demo(ax, Ri, gt, gt_err, f):
-            Ri = Ri[1:]
-            ax.errorbar(Ri, gt, yerr=gt_err, fmt='ko', ms=10)
-            ax.plot(Ri, f, 'r-', lw=3)
-            ax.set_xscale('log')
-            #for x, fi, gti, gei in izip(Ri, f, gt, gt_err):
-                #ax.annotate('{0:.2f}'.format((fi-gti)/gei),
-                            #xy=(x,gti+20), ha='center', va='bottom',
-                            #color='r')
-            return
-        val1[numpy.where(jfree)] = starting
-        if params_join is not None:
-            v1 = list(val1)
-            for p in params_join:
-                # without this list comprehension numpy can't keep track of the
-                # data type. I believe this is because there are elements of
-                # different types in val1 and therefore its type is not
-                # well defined (so it gets "object")
-                v1[p[0]] = array([val1[pj] for pj in p])
-            # need to delete elements backwards to preserve indices
-            aux = [[v1.pop(pj) for pj in p[1:][::-1]]
-                   for p in params_join[::-1]]
-            val1 = v1 #array(v1) ??
-        to = time()
-        model = function(val1, R)
-        print('Model evaluated in {0:.2e} seconds'.format(time() - to))
-        #residuals = esd - model[0]
-        if 'model' in str(function):
-            # model assumes comoving separations, changing data to accomodate for that
-            redshift_index = numpy.int(numpy.where(params == 'z')[0])
-            redshift = array([val1[redshift_index]])
-            esd = esd/(1.0+redshift.T)**2.0
-            residuals = (esd - model[0])*(1.0+redshift.T)**2.0
-            esd_err = esd_err/(1.0+redshift.T)**2.0
-        else:
-            residuals = esd - model[0]
+        run_demo(function, R, esd, esd_err, cov, val1, params_join, starting,
+                 params, rng_obsbins, Ndatafiles)
+        sys.exit()
 
-        dof = esd.size - starting.size - 1
-        chi2 = array([dot(residuals[m], dot(icov[m][n], residuals[n]))
-                    for m in rng_obsbins for n in rng_obsbins]).sum()
-
-        print(' ** chi2 = %.2f/%d **' %(chi2, dof))
-        fig, axes = pylab.subplots(figsize=(4*Ndatafiles,4), ncols=Ndatafiles)
-        if Ndatafiles == 1:
-            plot_demo(axes, R[0], esd[0], esd_err[0], model[0][0])
-        else:
-            for i in izip(axes, R, esd, esd_err, model[0]):
-                plot_demo(*i)
-        if npall(esd - esd_err > 0) or 'model' in str(function):
-            if Ndatafiles == 1:
-                axes.set_yscale('log')
-            else:
-                for ax in axes:
-                    ax.set_yscale('log')
-        #fig.tight_layout(w_pad=0.01)
-        pylab.show()
-        fig, axes = pylab.subplots(figsize=(10,8), nrows=cov.shape[0],
-                                   ncols=cov.shape[0])
-        vmin, vmax = numpy.percentile(cov, [1,99])
-        if Ndatafiles == 1:
-            for m in xrange(1):
-                for n in xrange(1):
-                    axes.imshow(cov[m][-n-1][::-1], interpolation='nearest',
-                            cmap=cm.CMRmap_r, vmin=vmin, vmax=vmax)
-        else:
-            for m, axm in enumerate(axes):
-                for n, axmn in enumerate(axm):
-                    axmn.imshow(cov[m][-n-1][::-1], interpolation='nearest',
-                            cmap=cm.CMRmap_r, vmin=vmin, vmax=vmax)
-        #fig.tight_layout()
-        pylab.show()
-        exit()
+    write_hdr(
+        output, datafile, datacols, covfile, covcols, exclude_bins,
+        params, prior_types, val1, val2, val3, val4,
+        nwalkers, nsteps, nburn, thin)
 
     # set up starting point for all walkers
     #po = starting * numpy.random.uniform(0.9, 1.1, size=(nwalkers,ndim))
@@ -455,6 +345,82 @@ def lnprob(theta, R, esd, icov, function, params, prior_types,
     return lnlike + lnprior_total, model
 
 
+def run_demo():
+    def plot_demo(ax, Ri, gt, gt_err, f):
+        Ri = Ri[1:]
+        ax.errorbar(Ri, gt, yerr=gt_err, fmt='ko', ms=10)
+        ax.plot(Ri, f, 'r-', lw=3)
+        ax.set_xscale('log')
+        #for x, fi, gti, gei in izip(Ri, f, gt, gt_err):
+            #ax.annotate('{0:.2f}'.format((fi-gti)/gei),
+                        #xy=(x,gti+20), ha='center', va='bottom',
+                        #color='r')
+        return
+    val1[numpy.where(jfree)] = starting
+    if params_join is not None:
+        v1 = list(val1)
+        for p in params_join:
+            # without this list comprehension numpy can't keep track of the
+            # data type. I believe this is because there are elements of
+            # different types in val1 and therefore its type is not
+            # well defined (so it gets "object")
+            v1[p[0]] = array([val1[pj] for pj in p])
+        # need to delete elements backwards to preserve indices
+        aux = [[v1.pop(pj) for pj in p[1:][::-1]]
+               for p in params_join[::-1]]
+        val1 = v1 #array(v1) ??
+    to = time()
+    model = function(val1, R)
+    print('Model evaluated in {0:.2e} seconds'.format(time() - to))
+    #residuals = esd - model[0]
+    if 'model' in str(function):
+        # model assumes comoving separations, changing data to accomodate for that
+        redshift_index = numpy.int(numpy.where(params == 'z')[0])
+        redshift = array([val1[redshift_index]])
+        esd = esd/(1.0+redshift.T)**2.0
+        residuals = (esd - model[0])*(1.0+redshift.T)**2.0
+        esd_err = esd_err/(1.0+redshift.T)**2.0
+    else:
+        residuals = esd - model[0]
+
+    dof = esd.size - starting.size - 1
+    chi2 = array([dot(residuals[m], dot(icov[m][n], residuals[n]))
+                  for m in rng_obsbins for n in rng_obsbins]).sum()
+
+    print(' ** chi2 = %.2f/%d **' %(chi2, dof))
+    fig, axes = pylab.subplots(figsize=(4*Ndatafiles,4), ncols=Ndatafiles)
+    if Ndatafiles == 1:
+        plot_demo(axes, R[0], esd[0], esd_err[0], model[0][0])
+    else:
+        for i in izip(axes, R, esd, esd_err, model[0]):
+            plot_demo(*i)
+    if npall(esd - esd_err > 0) or 'model' in str(function):
+        if Ndatafiles == 1:
+            axes.set_yscale('log')
+        else:
+            for ax in axes:
+                ax.set_yscale('log')
+    #fig.tight_layout(w_pad=0.01)
+    pylab.show()
+    fig, axes = pylab.subplots(
+        figsize=(10,8), nrows=cov.shape[0], ncols=cov.shape[0])
+    vmin, vmax = numpy.percentile(cov, [1,99])
+    if Ndatafiles == 1:
+        axes = [[axes]]
+        #for m in xrange(1):
+            #for n in xrange(1):
+                #axes.imshow(cov[m][-n-1][::-1], interpolation='nearest',
+                            #cmap=cm.CMRmap_r, vmin=vmin, vmax=vmax)
+    #else:
+    for m, axm in enumerate(axes):
+        for n, axmn in enumerate(axm):
+            axmn.imshow(cov[m][-n-1][::-1], interpolation='nearest',
+                        vmin=vmin, vmax=vmax)
+    #fig.tight_layout()
+    pylab.show()
+    return
+
+
 def write_to_fits(output, chi2, sampler, nwalkers, thin, params, jfree,
                   metadata, meta_names, fits_format, update_freq, iternum,
                   nwritten, Nobsbins, fail_value, array, BinTableHDU,
@@ -614,3 +580,47 @@ def sampler_ball(params, prior_types, jfree, starting, val1, val2, val3, val4,
             ball[:,n] = numpy.random.uniform(min, max, size=nwalkers)
 
     return ball
+
+
+def write_hdr(output, datafile, datacols, covfile, covcols, exclude_bins,
+              params, prior_types, val1, val2, val3, val4,
+              nwalkers, nsteps, nburn, thin):
+    hdrfile = '.'.join(output.split('.')[:-1]) + '.hdr'
+    print('Printing header information to', hdrfile)
+    hdr = open(hdrfile, 'w')
+    print('Started', ctime(), file=hdr)
+    print('datafile', ','.join(datafile), file=hdr)
+    print('cols', ','.join([str(c) for c in datacols]), file=hdr)
+    print('covfile', covfile, file=hdr)
+    print('covcols', ','.join([str(c) for c in covcols]), file=hdr)
+    if exclude_bins is not None:
+        print('exclude_bins', ','.join([str(c) for c in exclude_bins]),
+              file=hdr)
+    print('model {0}'.format(function), file=hdr)
+    for p, pt, v1, v2, v3, v4 in izip(params, prior_types,
+                                      val1, val2, val3, val4):
+        try:
+            line = '%s  %s  ' %(p, pt)
+            line += ','.join(numpy.array(v1, dtype=str))
+        except TypeError:
+            line = '%s  %s  %s' %(p, pt, str(v1))
+            if pt not in ('fixed', 'function', 'read'):
+                # I don't know why v2, v3 and v4 are single-element
+                # arrays instead of floats but who cares if all the rest
+                # works... I'm leaving the try statement just in case they
+                # might be floats at some point
+                try:
+                    line += '  %s  %s  %s' \
+                            %tuple([str(v[0]) for v in (v2,v3,v4)])
+                except TypeError:
+                    line += '  %s  %s  %s' \
+                            %tuple([str(v) for v in (v2,v3,v4)])
+        print(line, file=hdr)
+    print('nwalkers  {0:5d}'.format(nwalkers), file=hdr)
+    print('nsteps    {0:5d}'.format(nsteps), file=hdr)
+    print('nburn     {0:5d}'.format(nburn), file=hdr)
+    print('thin      {0:5d}'.format(thin), file=hdr)
+    hdr.close()
+    return
+
+

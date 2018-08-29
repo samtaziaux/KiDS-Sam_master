3fa92523d18eb436ec202241795f203b9d450acb
cristobal-sifon <sifon@strw.leidenuniv.nl>
2018-08-29 21:48:00 +0200

    removed unnecessary commented code

--- kids_ggl_pipeline/halomodel/concentration.py
+++ kids_ggl_pipeline/halomodel/concentration.py
@@ -10,25 +10,6 @@ from ..helpers.functions import powerlaw, powerlaw_mz
 ## Default relations
 ##
 
-"""
-def duffy08_crit(M, z, f, h=1):
-    return f * powerlaw_mz(M, z, 12.301-log10(h), 0, 6.71, -0.091, -0.44)
-
-
-def duffy08_mean(M, z, f, h=1):
-    return f * powerlaw_mz(M, z, 12.301-log10(h), 0, 10.14, -0.081, -1.01)
-
-
-def dutton14(M, f, h=1):
-    return f * powerlaw(M, 12-log10(h), 8.035, -0.101)
-"""
-
-"""
-These below serve as a placeholder until we implement the ability to
-use the same parameter in multiple places, at which point the
-z[:,newaxis] operation will probably happen in halo.model
-"""
-
 
 def duffy08_crit(M, z, f, h=1):
     return f * powerlaw_mz(

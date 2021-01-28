0e817d92504453a73500cf6b1050104a688fe121
Andrej Dvornik <andrej.dvornik@me.com>
2021-01-28 11:46:55 +0100

    Fix sphinx

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -41,6 +41,7 @@ release = '2.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme',
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.todo',
@@ -88,6 +89,7 @@ pygments_style = 'sphinx'
 #
 #html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

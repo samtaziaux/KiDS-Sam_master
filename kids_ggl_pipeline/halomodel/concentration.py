395a85930ea6205200c8ae022ed427f1940cebdf
Cristóbal Sifón <cristobal.sifon@pucv.cl>
2021-06-17 13:55:48 -0400

    fix bug in relations without redshift

--- kids_ggl_pipeline/halomodel/concentration.py
+++ kids_ggl_pipeline/halomodel/concentration.py
@@ -3,12 +3,13 @@ from __future__ import (absolute_import, division, print_function,
 
 from numpy import log10
 
-from ..helpers.functions import *
+from ..helpers.functions import powerlaw, powerlaw_mz
 
 
-##
-## Default relations
-##
+"""Default relations for the mass-concentration relation"""
+
+# Note that relations that do not include redshift must add a leading
+# empty dimension (see, e.g., dutton14)
 
 
 def duffy08_crit(M, z, f, h=1):
@@ -25,12 +26,13 @@ def duffy08_mean(M, z, f, h=1):
 
 def dutton14(M, f, h=1):
     """At z=0"""
-    return f * powerlaw(M, 12, 0.905, -0.101, return_log=False) # h-factor already accounted for in the M, cancels out!
+    # h-factor already accounted for in the M, cancels out!
+    return f * powerlaw(M, 12, 0.905, -0.101, return_log=False)[None]
     #return f * powerlaw(M, 12-log10(h), 0.905, -0.101, return_log=False)
 
 
 def cm_powerlaw(M, logM0, a, b):
-    return powerlaw(M, logM0, a, b, return_log=False)
+    return powerlaw(M, logM0, a, b, return_log=False)[None]
 
 
 def cmz_powerlaw(M, z, logM0, z0, a, b, c):
